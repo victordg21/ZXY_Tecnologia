2d0b269ee48365372e1478af8842836a44f577ea
Joao Victor Dias <victordg050@gmail.com>
2023-04-18 10:59:13 -0300

    Consertando responsividade da seção 'Sobre Nós'

--- src/components/Home/About.vue
+++ src/components/Home/About.vue
@@ -3,30 +3,37 @@
         <div class="container">
             <h2 class="topic">Sobre Nós</h2>
             <div class="row">
-                <img src="../../assets/images/foto-sobre-nos.png" alt="">
+                <img class="col-lg-12 image" src="../../assets/images/foto-sobre-nos.png" alt="">
             </div>
             <div class="row">
-                <div class="col-4 text-left">
-                    <h3 class="subtopic">Missão</h3>
-                    <p class="paragraph">A missão da ZXY Tecnologia é fornecer serviços na área de TI de forma excepcional, 
-                        ajudando nossos clientes a alcançar o sucesso em suas iniciativas tecnológicas. Nós nos dedicamos 
-                        a entender as necessidades específicas de cada cliente e fornecer soluções personalizadas, 
-                        inovadoras e eficientes, sempre buscando a excelência na qualidade de nossos serviços.</p>
+                <div class="col-4">
+                    <h3 class="subtopic text">Missão</h3>
+                    <p class="paragraph text-left">A missão da ZXY Tecnologia é transformar a maneira como o mundo se conecta, simplificando 
+                        e otimizando processos por meio de soluções tecnológicas inovadoras. Buscamos constantemente superar os limites do 
+                        possível, desenvolvendo produtos e serviços que impulsionam a eficiência, a produtividade e o progresso. Com um 
+                        compromisso inabalável com a qualidade e o atendimento ao cliente, nossa missão é ser uma referência no mercado, 
+                        fornecendo soluções tecnológicas confiáveis e de ponta para nossos clientes, ajudando-os a alcançar o sucesso em 
+                        um mundo cada vez mais digital.</p>
                 </div>
-                <div class="col-4 text-center">
-                    <h3 class="subtopic">Visão</h3>
-                    <p class="paragraph">A missão da ZXY Tecnologia é fornecer serviços na área de TI de forma excepcional, 
-                        ajudando nossos clientes a alcançar o sucesso em suas iniciativas tecnológicas. Nós nos dedicamos 
-                        a entender as necessidades específicas de cada cliente e fornecer soluções personalizadas, 
-                        inovadoras e eficientes, sempre buscando a excelência na qualidade de nossos serviços.</p>
+                <div class="col-4">
+                    <h3 class="subtopic text">Visão</h3>
+                    <p class="paragraph text-center">A ZXY Tecnologia enxerga um mundo onde a inovação é a força motriz para o progresso. 
+                        Com uma visão centrada no futuro, a empresa busca constantemente criar soluções tecnológicas 
+                        revolucionárias que transformam a maneira como vivemos e trabalhamos. Com uma equipe apaixonada por 
+                        desafios e comprometida com a excelência, a ZXY Tecnologia busca ser líder no mercado, impulsionando 
+                        a evolução tecnológica e criando um impacto positivo na sociedade. Com base em uma abordagem colaborativa 
+                        e orientada para o cliente, a visão da ZXY Tecnologia é criar um futuro mais brilhante, conectado e 
+                        eficiente por meio da inovação tecnológica.</p>
                 </div>
-                <div class="col-4 text-right">
-                    <h3 class="subtopic">Valores</h3>
-                    <p class="paragraph">1. <b>Excelência</b>: Buscamos constantemente a excelência em tudo o que fazemos, 
-                        desde o atendimento ao cliente até a entrega final dos projetos.</p>
-                    <p class="paragraph">2. <b>Inovação</b>: Acreditamos que a inovação é a chave para o sucesso em um mercado 
-                        em constante evolução. Buscamos sempre estar atualizados e trabalhamos em conjunto com nossos clientes 
-                        para desenvolver soluções inovadoras e criativas.</p>
+                <div class="col-4">
+                    <h3 class="subtopic text">Valores</h3>
+                    <p class="paragraph text-right">Os valores fundamentais da ZXY Tecnologia são a inovação, integridade, excelência e 
+                        colaboração. Acreditamos que a inovação é a chave para criar soluções tecnológicas disruptivas e impactar 
+                        positivamente a sociedade. Valorizamos a integridade em todas as nossas ações, mantendo altos padrões 
+                        éticos e transparência em nossos negócios. Buscamos constantemente a excelência em tudo o que fazemos, 
+                        desde o desenvolvimento de produtos até o atendimento ao cliente. E, por fim, acreditamos na colaboração 
+                        como uma maneira poderosa de alcançar resultados excepcionais, trabalhando em equipe com nossos clientes, 
+                        parceiros e colaboradores. Esses valores são a base que impulsiona a cultura e o sucesso da ZXY Tecnologia.</p>
                 </div>
             </div>
         </div>
@@ -48,9 +55,16 @@
         text-align: center;
         font-weight: 600;
     }
-
     .text-center{
         text-align: center;
+    }
+
+    .text-left{
+        text-align: left;
+    }
+
+    .text-right{
+        text-align: right;
     }
     .subtopic{
         font-weight: 600;
